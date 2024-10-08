3b3648e0f3fa2807cee33ef544c4531733466839
HoussaineBenDhieb <131673785+houssainebendhieb@users.noreply.github.com>
2024-10-08 17:10:15 +0100

    add item listview

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly/core/utils/assets.dart';
 import 'package:bookly/features/home/presentation/views/widgets/custom_navbar.dart';
 import 'package:flutter/material.dart';
 
@@ -9,9 +10,28 @@ class HomeViewBody extends StatelessWidget {
     return const Column(
       children: [
         CustomAppBar(),
+        CustomListViewItem(),
       ],
     );
   }
 }
 
-
+class CustomListViewItem extends StatelessWidget {
+  const CustomListViewItem({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * 0.3,
+      child: AspectRatio(
+        aspectRatio: 2.7 / 4,
+        child: Container(
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(16),
+              color: Colors.orange,
+              image: const DecorationImage(
+                  fit: BoxFit.fill, image: AssetImage(AssetsData.logo))),
+        ),
+      ),
+    );
+  }
+}
