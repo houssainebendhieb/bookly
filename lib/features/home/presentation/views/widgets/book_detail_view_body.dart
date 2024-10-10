134bf1bb357b017226de7ce1674570b263a2474e
HoussaineBenDhieb <131673785+houssainebendhieb@users.noreply.github.com>
2024-10-10 13:20:34 +0100

    add custom scroll view to book detail

--- lib/features/home/presentation/views/widgets/book_detail_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_detail_view_body.dart
@@ -11,56 +11,67 @@ class BookDetailViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 30),
-      child: Column(
-        children: [
-          const CustomAppBarBookDetail(),
-          Padding(
-            padding: EdgeInsets.symmetric(
-                horizontal: MediaQuery.of(context).size.width * 0.15),
-            child: const CustomListViewItem(),
+    return CustomScrollView(
+      slivers: [
+        SliverFillRemaining(
+          
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30),
+            child: Column(
+              children: [
+                const CustomAppBarBookDetail(),
+                Padding(
+                  padding: EdgeInsets.symmetric(
+                      horizontal: MediaQuery.of(context).size.width * 0.15),
+                  child: const CustomListViewItem(),
+                ),
+                const SizedBox(
+                  height: 43,
+                ),
+                Text("The jungle Book",
+                    style: Styles.textStyle30
+                        .copyWith(fontWeight: FontWeight.normal)),
+                const SizedBox(
+                  height: 3,
+                ),
+                Opacity(
+                  opacity: 0.7,
+                  child: Text("harry potter book",
+                      style: Styles.textStyle18.copyWith(
+                          fontStyle: FontStyle.italic,
+                          fontWeight: FontWeight.w500)),
+                ),
+                const SizedBox(
+                  height: 18,
+                ),
+                const BookRating(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                ),
+                const BookAction(),
+                const Expanded(
+                  child: SizedBox(
+                    height: 50,
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text("you can also see ",
+                      style: Styles.textStyle18
+                          .copyWith(fontWeight: FontWeight.bold)),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                const SimilarBooksListView(),
+                const SimilarBooksListView(),
+                const SizedBox(
+                  height: 15,
+                )
+              ],
+            ),
           ),
-          const SizedBox(
-            height: 43,
-          ),
-          Text("The jungle Book",
-              style:
-                  Styles.textStyle30.copyWith(fontWeight: FontWeight.normal)),
-          const SizedBox(
-            height: 3,
-          ),
-          Opacity(
-            opacity: 0.7,
-            child: Text("harry potter book",
-                style: Styles.textStyle18.copyWith(
-                    fontStyle: FontStyle.italic, fontWeight: FontWeight.w500)),
-          ),
-          const SizedBox(
-            height: 18,
-          ),
-          const BookRating(
-            mainAxisAlignment: MainAxisAlignment.center,
-          ),
-          const BookAction(),
-          const SizedBox(
-            height: 30,
-          ),
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Text("you can also see ",
-                style:
-                    Styles.textStyle18.copyWith(fontWeight: FontWeight.bold)),
-          ),
-          const SizedBox(
-            height: 15,
-          ),
-          const SimilarBooksListView(),
-          const SizedBox(
-            height: 15,
-          )
-        ],
-      ),
+        )
+      ],
     );
   }
 }
